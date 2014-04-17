dfa221a70be08d083ac7199aeeb6ea66faceff13
Ian <ian.coates@gmail.com>
2014-04-17 12:32:30 +0100

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -2,4 +2,13 @@
 
 // may change the name of this file later
 
+function create_deeplink($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $deeplink = '';
+    for ($i = 0; $i < $length; $i++) {
+        $deeplink; .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $deeplink;
+}
+
 ?>
