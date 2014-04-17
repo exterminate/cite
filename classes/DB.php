f13d7b7b661a10ae3ad48b638fefe4f91ccd1556
Ian <ian.coates@gmail.com>
2014-04-17 12:54:48 +0100

    update DB.php

--- classes/DB.php
+++ classes/DB.php
@@ -1,7 +1,14 @@
 <?php
 
 class DB {
+	
 	public function __construct() {
   
 	}
 }
+
+/* 
+*  COLUMNS NEEDED
+*  id, linkid, name, email, orcid (required), datesubmitted, doi, datedoi
+*  any more?
+*/
