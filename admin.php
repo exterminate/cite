057225b7d2e8bf11217454561ff241604bde8265
Ian <ian@Ian-Coatess-iMac.local>
2014-05-07 18:51:12 +0100

    updates to admin and User class

--- admin.php
+++ admin.php
@@ -1,5 +1,23 @@
 <?php
+session_start();
+require 'classes/User.php';
+//require 'classes/Stub.php';
+require 'classes/Input.php';
+require 'classes/Validate.php';
+require 'includes/functions.php';
 
+if(Input::exists()) {
+	try {
+		$handler = new PDO('mysql:host=127.0.0.1;dbname=cite', 'root', 'root');
+		$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e) {
+		echo $e->getMessage();
+		die("sorry, database problem");
+	}
+
+	$user = new User(Input::get('username'),Input::get('password'));
+
+}
 // login to admin so we can delete spam,
 // fix broken links etc.
 
--- classes/User.php
+++ classes/User.php
@@ -2,17 +2,36 @@
 
 class User {
 
-	private $_db;
-
-	public function __construct($user = null) {
-		//$this->_db = DB::getInstance();
+	public function __construct($handler, $user, $password) {
+		$this->handler = $handler;
+		$this->user = $user;
+		$this->password = md5($password);
 	}
-/*
-	public function create($fields = array()) {
-		if($this->_db->insert('users', $fields)) {
-			throw new Exception('There was a problem creating an account.');
+
+	public function login() {
+		$query = $this->handler->query("SELECT * FROM users WHERE username = '$this->user'");
+		if($query->rowCount() == 1) { // check user exists
+			// Success user exists, is password ok?
+			$r = $query->fetch(PDO::FETCH_OBJ);
+			if($r->password == $this->password) { 
+				//password ok
+				if($r->password.$r->clue == $this->password.md5($this->username) { 
+					//great let's start the session
+					$sql = "UPDATE users SET lastlogin = NOW() WHERE username = ?";
+					$query = $this->handler->prepare($sql);
+					$query->execute(array($this->username));
+					session_start();
+					$_SESSION['username'] = $this->username;
+				}
+			} else {
+				//password fail
+				die("This user exists but you got the wrong password!");
+			}
+
+
+		} else { // user don't exist
+			die("This user doesn't exist!");
 		}
 	}
-*/
-	//public static function
+
 }
