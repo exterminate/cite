8f231e5c639af00641b852c1991fdfe93595c398
Ian <ian.coates@gmail.com>
2014-05-16 09:29:33 +0100

    update

--- search.php
+++ search.php
@@ -1,3 +1,12 @@
 <?php
 //search page
+require 'core/init.php';
+
+
 ?>
+
+<form>
+<label for='search'>Enter ORCID</label>
+<input type='text' name='orcid' id='search'>
+<!-- Add human checking tool -->
+</form>
