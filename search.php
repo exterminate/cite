0cc51e73c1b38feb03a394239ea1ca29af987578
Ian <ian.coates@gmail.com>
2014-05-16 12:32:00 +0100

    currently creating search.php

--- search.php
+++ search.php
@@ -1,12 +1,38 @@
 <?php
 //search page
+// Should probably add this to the index page <= I'll just get it working first :Ian
+
 require 'core/init.php';
 
-
+if(Input::exists()) {
+  
+  $validate = new Validate();
+	$validate->check($_POST, array( 
+		'orcid' => array(
+			'required' 	=> true,
+			'orcid' => true
+			)
+		)
+	);
+	
+	if($validate->passed()) {
+	  try {
+	    // let's get some search results!
+	    
+	 
+	     
+	  } catch(Exception $e) {
+			die($e->getMessage());
+		}
+	 
+	}
+	
+}
 ?>
 
 <form>
 <label for='search'>Enter ORCID</label>
 <input type='text' name='orcid' id='search'>
 <!-- Add human checking tool -->
+<input type='submit' name='submit' id='submit' value='Search'>
 </form>
