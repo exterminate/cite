85baeca78ca6bd9b8602389c7284581d25b7916d
Ian <ian.coates@gmail.com>
2014-05-20 12:39:31 +0100

    added nav

--- layout/header.php
+++ layout/header.php
@@ -2,5 +2,9 @@
 	<div class="headerwrap">
 	    <h1 class="header">Cite</h1>
 	</div>
+	<nav>
+	    <a href="/submit/">Submit stub</a>
+	    <a href="/search/">Search</a>
+	</nav>
 </header>
 <div class="wrapper">
