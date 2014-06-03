f9225b32b7ae99ca59887e09e4ad9d422327e124
Matthew Smith <matthew.smith813@gmail.com>
2014-06-03 21:58:17 +0100

    Updated search with dropdown for field
    Also added admin true/false on search

--- search.php
+++ search.php
@@ -11,64 +11,90 @@ include 'layout/header.php';
 	<script src="../lib/jquery.maskedinput.js" type="text/javascript"></script>
 	<script>
 		$(document).ready(function(){
-			$('#orcid').mask("9999-9999-9999-9999", {placeholder : ".", completed: function(){
-				//alert("finished typing orcid");
-			}});
+			$('#typeSelect').change(function(){
+				console.log($(this).val());
+				if($(this).val() == "orcid"){
+					$('#searchQuery').mask("9999-9999-9999-9999", {placeholder : ".", completed: function(){
+						searchDB($(this).val(), "orcid");
+					}});
+				} else{
+					$('#searchQuery').unmask();
+				}
+			});
 
-			$('#submit').click(function(){
-
-				var query = $('#orcid').val();				
-
-				$.post("../searchSQL.php", {query : query, type : "orcid"}, function(data){
-					console.log(data);
-					$('#output').empty();
-					printJSONToTable(data, $('#output'));	
-				})
-				.fail(function(a,b,c){
-					console.log("Error contacting server: " + a.responseText + b + ", " + c);
-				});
+			$('#searchButton').click(function(){
+				var query = $('#searchQuery').val();
+				var type = $('#typeSelect').val();
+				searchDB(query, type);
 			});
 		});
+
+		function searchDB(query, type){
+			/*
+				Note this only finds exact matches at present
+			*/
+			$.post("../searchSQL.php", {query : query, type : type, admin : false}, function(data){
+				console.log(data);
+				$('#output').empty();
+				printJSONToTable(data, $('#output'));	
+			})
+			.fail(function(a,b,c){
+				console.log("Error contacting server: " + a.responseText + b + ", " + c);
+			});
+	}
 
 		function printJSONToTable(data, outputElement){
 			var table = $('<table>').attr("class", "table").attr("id", "resultsTable");
 
-			var length = Object.keys(data).length;
+			if(data.error != null){
+				$(outputElement).append("Error: " + data.error);
+			} else {
+				var length = Object.keys(data).length;
 
-			//populate the header rows
-			var headerRow = $("<tr>");
-			$.each(data[0], function(key, val){				
-				$(headerRow).append($("<th>").text(key));
-			})
+				//populate the header row
+				var headerRow = $("<tr>");
+				$.each(data[0], function(key, val){				
+					$(headerRow).append($("<th>").text(key));
+				})
 
-			$(table).append($(headerRow));
+				$(table).append($(headerRow));
 
-			//populate the body rows
-			for(var i = 0; i < length; i++){
-				var row = $("<tr>");
+				//populate the body rows
+				for(var i = 0; i < length; i++){
+					var row = $("<tr>");
 
-				$.each(data[i], function(key, val){
-					var cell = $("<td>").text(val);
-					$(row).append($(cell));
-				});				
+					$.each(data[i], function(key, val){
+						var cell = $("<td>").text(val);
+						$(row).append($(cell));
+					});				
 
-				$(table).append($(row))
+					$(table).append($(row))
+				}
+
+
+				$(outputElement).append($(table));		
 			}
-
-
-			$(outputElement).append($(table));		
 		}
 			
 	</script>
 
 	<div>
-		<label for='orcid'>Enter ORCID
-			<input type='text' name='orcid' id='orcid'>
-		</label>		
+		<form>			
+			<label for='searchQuery'>Search for:
+				<input type='text' name='searchQuery' id='searchQuery'>
+				<button id='searchButton' type='button'>Search</button>
+				<select id='typeSelect'>
+					<option value='email'>Email</option>
+					<option value='name'>Name</option>
+					<option value='orcid'>Orcid</option>
+					<option value='deeplink'>Stub ID</option>					
+				</select>
+			</label>	
 
-		<button id='submit'>Submit</button>
+			
+		</form>
 	</div>
-
+	<br/>
 	<div id='output'>
 	</div>
 
--- searchSQL.php
+++ searchSQL.php
@@ -6,27 +6,61 @@
     PhpConsole\Helper::register();
 	require 'core/init.php';
 
-    $orcid = Input::get('query');
+    $query = Input::get('query');
     $type = Input::get('type');
+    $admin = Input::get('admin');
+
+    //PC::debug($query.", ".$type);
 
   	try {
 	    // let's get some search results!
 	    $stub = new Stub($handler);
-	    $results = $stub->obtainData($type, $orcid); //fetches array(?) :Ian
+	    $results = $stub->obtainData($type, $query); //fetches array(?) :Ian
 
-	    //each array in $results is 2 arrays back to back, one numerically indexed, the other associatively indexed
-	    //so we will split the array in half and return only the associative part
+	    //PC::debug($results);
 
-	    $toReturn = array();
-	    foreach($results as $key => $val){
-	    	$temp = array();
-	    	foreach($val as $x => $y){
-	    		if(!is_int($x)){
-	    			$temp[$x] = $y;
-	    		}
-	    	}
-			array_push($toReturn, $temp);
-	    }
+	    if($results == null){  
+
+	    	$toReturn = array("error" => "No results found matching ".$type." = ".$query);
+
+	    }else{
+
+	    	if($admin == 'true'){
+	    		/*
+					Return the whole table if admin is true.
+					Each array in $results is 2 arrays back to back, one numerically indexed, the other associatively indexed
+		    		so we will split the array in half and return only the associative part
+	    		*/
+	    		$toReturn = array();
+		   		foreach($results as $key => $val){
+			    	$temp = array();
+			    	foreach($val as $x => $y){
+			    		if(!is_int($x)){
+			    			$temp[$x] = $y;
+			    		}
+		    		}
+					array_push($toReturn, $temp);
+				}
+		    } else{
+		    	/*
+		    		Return only the publically visible part of the stub if not an admin search
+		    	*/
+		    	$toReturn = array();
+		    	foreach($results as $key => $val){
+		    		$temp = array();   		
+			    	$temp['Name'] = $val['name'];
+			    	$temp['Email'] = $val['email'];
+			    	$temp['Orcid'] = $val['orcid'];
+		    		$temp['Description'] = $val['description'];
+		    		$temp['Date Created'] = $val['datesubmitted'];
+		    		$temp['DOI'] = $val['doi'];
+		    		$temp['Date Completed'] = $val['datedoi'];
+		    		$temp['Stub ID'] = $val['deeplink'];
+			    	
+			    	array_push($toReturn, $temp);
+		    	}
+		    }
+		} 
 
 	    //PC::debug($toReturn);
 	    echo json_encode($toReturn, JSON_FORCE_OBJECT);		    
