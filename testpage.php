6b3f598f2424871301ec1959fe4551c5ade3e70c
Ian <ian.coates@gmail.com>
2014-06-04 18:40:48 +0100

    updates to method names

--- testpage.php
+++ testpage.php
@@ -10,14 +10,14 @@ echo "<pre>";
 
 // returns array
 // (table, column, specific)
-print_r( $db->read('links','deeplink','b9s0aJPq') ); 
+print_r( $db->get('links','deeplink','b9s0aJPq') ); 
 echo "</pre>";
 
 echo "<br>";
 
 // returns first item in array once read is called
 // alternatively, you could do $db->first()['name'];
-echo $db->first('name'); 
+echo $db->getFirst('name'); 
 
 
 echo "<br>";
