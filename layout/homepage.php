89f4fe55cc48ca2edbad6e8f48ba0fd0d908aa38
Ian <ian.coates@gmail.com>
2014-06-05 18:08:26 +0100

    added more info

--- layout/homepage.php
+++ layout/homepage.php
@@ -4,6 +4,13 @@
 	</div>
 
 	<div class="bodytext">
+		<p>Want to reference a work that you haven't quite put the finishing touches on yet? Or that seminal paper you will write in five years time after years of groundbreaking research? You can do it easily using Cite.pub!</p>
+		<ol class='circles-list'>
+		  	<li>Create a stub on Cite.pub using your Orcid ID</li>
+			<li>Cite the unique URL in your publication</li>
+		  	<li>Publish your seminal work</li>
+		  	<li>Complete your stub with the DOI of the new paper. The original URL will forward readers straight through!</li>
+		</ol>
 		<p>Would you like to cite future work you discuss in your current paper but have not yet written up or possibly not even done?</p>
 		<p>If you use Cite you can create a DOI-like stub that will stay online until you are ready to publish. Once you do, update your record and we will redirect the reader straight to the publisher where the paper is published.</p>
 	</div>
