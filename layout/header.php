4cdcadf98cfcb0553aed22cb03f772a1df35eb45
Ian <ian@iancoatessimac.lan>
2014-06-08 10:35:17 +0100

    update

--- layout/header.php
+++ layout/header.php
@@ -5,9 +5,9 @@
 				<h1 class="header">Cite</h1>
 			</a>
 			<nav>
-			    <a href="<?php echo $rootURL; ?>submit/">Submit stub</a>
-			    <a href="<?php echo $rootURL; ?>search/">Search</a>
-			    <a href="<?php echo $rootURL; ?>about/">About</a>
+			    <a href="<?php echo $rootURL; ?>submit">Submit stub</a>
+			    <a href="<?php echo $rootURL; ?>search">Search</a>
+			    <a href="<?php echo $rootURL; ?>about">About</a>
 			</nav>
 		</div>
 	</header>
