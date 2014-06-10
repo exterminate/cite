6f6f9df9b6e470f9c70a5ba3df920ae1aa4162e3
Matthew Smith <matthew.smith813@gmail.com>
2014-06-10 22:41:01 +0100

    readding core after .gitignore experiments

---
+++ core/init.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'classes/User.php';
+require 'classes/DB.php';
+require 'classes/Stub.php';
+require 'classes/Input.php';
+require 'includes/functions.php';
+require 'classes/Validate.php';
+
+// modify to get links to work
+$rootURL = "http://localhost/cite/";
+
+try {
+	$handler = new PDO('mysql:host=127.0.0.1;dbname=cite', 'root', 'root');
+	$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+	echo $e->getMessage();
+	die("sorry, database problem");
+}
+
+$dbHandler = new DB($handler);
+
+?>
