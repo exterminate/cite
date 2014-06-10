521115f398685ce291f48f50ae162103c4bf5447
Ian <ian.coates@gmail.com>
2014-06-10 14:12:00 +0100

    added edit page

--- layout/header.php
+++ layout/header.php
@@ -5,9 +5,11 @@
 				<h1 class="header">Cite</h1>
 			</a>
 			<nav>
+			    <a href="<?php echo $rootURL; ?>">Home</a>
+			    <a href="<?php echo $rootURL; ?>about/">About</a>
 			    <a href="<?php echo $rootURL; ?>submit/">Submit stub</a>
 			    <a href="<?php echo $rootURL; ?>search/">Search</a>
-			    <a href="<?php echo $rootURL; ?>about/">About</a>
+			    <a href="<?php echo $rootURL; ?>edit/">edit</a>
 			</nav>
 		</div>
 	</header>
