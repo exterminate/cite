a70475b6960a87116d85b9a66f5a1f56fa9083e1
Ian <ian.coates@gmail.com>
2014-06-11 12:41:10 +0100

    create author class

--- classes/author.php
+++ classes/author.php
@@ -1,8 +1,26 @@
 <?php
 
 class Author {
-
-
+  
+  public $email;
+  public $deepLink;
+  public $time;
+  
+  function __construct($email, $deepLink, $handler) {
+    $this->email = $email;
+    $this->deepLink = $deepLink;
+    $this->handler = $handler;
+  }
+  
+  public function createLoginSession() {
+    $sql = "INSERT INTO authors () VALUES ()";
+    $array = array(
+      ":email" => $this->email,
+      ":deepLink" => $this->deepLink,
+      ":time" => strtotime("now")
+      );
+      
+  }
 }
 
 ?>
