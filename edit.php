c10265dce20845b49a3c63de802a096c0f9abb69
Ian <ian.coates@gmail.com>
2014-06-11 19:51:26 +0100

    update not complete, battery running out

--- edit.php
+++ edit.php
@@ -5,10 +5,11 @@ require 'classes/author.php';
 header('Content-type: application/json');
 
 $stubEmail = $_POST['stubEmail'];
+$validate = new Validate();
 
 if(isset($_POST['inputEmail']) && isset(Input::get('code'))){
 	//if email exists in the database
-	$validate = new Validate();
+	
 	$validate->check($_POST, array(  
 		'inputEmail' => array(
 			'required' 	=> true,
@@ -26,16 +27,36 @@ if(isset($_POST['inputEmail']) && isset(Input::get('code'))){
 	if($validate->passed()) {
 		$getStub = $dbHandler->getStub('email', escape(trim($_POST['inputEmail'])));
 		if($getStub != null) {
-			echo json_encode(array('emailValid' => 'true')); // Hey, we got the email
 			// let's create a deeplink for the author
-			
 			$author = new Author(escape(trim($_POST['inputEmail'])), $dbHandler->getUniqueCode('deepLink','authors'), $dbHandler);
 			$author->createLoginSession();
+			
+			echo json_encode(array('emailValid' => 'true')); // Hey, we got the email
 		} else
 			echo json_encode(array('emailValid' => 'false')); // Uh oh, we don't got the email
 	}
 		
-elseif(isset($_POST['inputEmail']) && !isset(Input::get('code'))){
+elseif(isset(Input::get('inputEmail')) && !isset(Input::get('code'))){
+	$validate->check($_POST, array(  
+	'inputEmail' => array(
+		'required' => true,
+		'max' => 80,
+		'email' => true			
+	));
+	if($validate->passed()) {
+		// let's create a deeplink for the author
+		$author = new Author(escape(trim($_POST['inputEmail'])), $dbHandler->getUniqueCode('deepLink','authors'), $dbHandler);
+		$author->createLoginSession();
+			
+		
+		$email = new EmailHandler();
+		$email->sendMail(
+			Input::get('email'),
+			"Code to edit your stub",
+			""
+		);
+	}
+
 	//if code is valid
 		echo json_encode(array('login' => 'true'));
 	//else 
