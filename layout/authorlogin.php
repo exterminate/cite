2c79eefbea5f40224a534349c2c9645f1ed9f901
Ian <ian.coates@gmail.com>
2014-06-11 12:10:52 +0100

    create author login

---
+++ layout/authorlogin.php
@@ -0,0 +1,21 @@
+<p>Request a one-time code. This will be sent to your inbox. </p>
+
+<p><a href="">Get code</a></p>
+
+<form action="" method="post">
+	<table>
+		<tr>
+			<td><label for="email">Email address</label></td>
+			<td><input type="email" name="email" id="email"></td>
+			<td><a href="">Get code</a></td>
+		<tr>	
+		<tr>
+			<td><label for="deepLink">Enter code</label></td>
+			<td><input type="text" name="deepLink" id="deepLink"></td>
+		<tr>				
+		<tr>
+			<td>&nbsp;</td>
+			<td><input type="submit" name="submit" value="Edit"></td>
+		</tr>
+	</table>		
+</form>
