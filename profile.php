22d53b36dbb6bb908656ad4f349d0f909949a856
Ian <ian.coates@gmail.com>
2014-06-22 21:00:34 +0100

    still working on profile

--- profile.php
+++ profile.php
@@ -15,7 +15,22 @@ if(isset($_SESSION['name']) && !isset(Input::get('orcid'))) {
 	
 } elseif(isset(Input::get('orcid'))) {
 	// see someone else's profile
-	
+	$validate = new Validate();
+	$validate->check($_GET, array( 
+                'orcid' => array(
+			'required' 	=> true,
+			'orcid' 	=> true			
+			)
+		)
+	);
+	if($validate->passed()) {
+		$user = $dbHandler->getUser('orcid', Input::get('orcid'));
+		echo "<strong>Profile: </strong>".$_SESSION['name']."<br>\n";
+		echo "<strong>E-mail: </strong>".$user->email."<br>\n";
+		echo "<p>You have ".count($table, $field, $operator, $code)." stubs";
+	} else {
+		echo "You have not entered a valid OrcID.";
+	}
 } else {
 	echo "You are not logged in.";
 }
