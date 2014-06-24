7c7b9f137788f45bbd36fbfafc85d6a362520443
Ian <ian@Ian-Coatess-iMac.local>
2014-06-24 19:28:06 +0100

    email interest should work

--- registerInterest.php
+++ registerInterest.php
@@ -1,26 +1,51 @@
 <?php
     
-    require 'core/init.php';
+require 'core/init.php';
+
+header('Content-type: application/json');
+
+if(Input::exists()) {
+
+    $validate = new Validate();
+    $validate->check($_POST, array( 
+            'interestedEmail' => array(
+                    'required' 	=> true,
+                    'email' 	=> true			
+                    ),
+            'stubId' => array(
+                    'required' 	=> true,
+                    'min' 	=> 8,
+                    'max' 	=> 8	
+                    )
+            )
+    );
     
-    header('Content-type: application/json');
-    
-    $stubId = Input::get('stubId');
-    $interestedEmail = Input::get('interestedEmail');;
-    
-    if(isset($stubId) && isset($interestedEmail)){
+    if($validate->passed()) {
+        
+        $stubId = Input::get('stubId');
+        $interestedEmail = Input::get('interestedEmail');
+
         //get the appropriate stub from the database using the stubId posted in
         $stub = $dbHandler->getStub("stubId", $stubId);
         
+        // unserialize the email array
+        $array = unserialize($stub->emailUpdate);
+        
         //append the interested email to the stub
-        array_push($stub->interestedEmails, $interestedEmail);
+        array_push($array, $interestedEmail);
         
         //write the stub back to the database
+        $stub->update('links', 'emailUpdate', serialize($array));
         
         //if db write is successful
         echo json_encode(array("interestRegistered" => true));
         
         //if db write fails
         //echo json_encode(array("interestRegistered" => false, "errorMsg" => "Failed to write email to database"));
+    
+    } else {
+        echo json_encode(array("interestRegistered" => false, "errorMsg" => "Failed to write email to database"));
     }
+}
 
 ?>
