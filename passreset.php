101b58f319a68e38369a54203d1d757cfdf49009
Ian <ian.coates@gmail.com>
2014-07-01 20:05:48 +0100

    currently writing passreset

--- passreset.php
+++ passreset.php
@@ -4,6 +4,24 @@ require 'core/init.php';
 include 'layout/head.php';
 include 'layout/header.php';
 
+if(Input::exists()) {
+	$validate = new Validate();
+	
+}
+
+?>
+<h2>Reset password</h2>
+<p>We will e-mail you with a new password.</p>
+
+<form action="" method="POST">
+	<label for="email">Enter e-mail address:</label><br>
+	<input type="email" name="email"><br>
+	<label for="human">What is 3 + 2?</label><br>
+	<input type="text" name="human"><br>
+	<input type="submit" name="reset" value="Reset"><br>
+</form>	
+
+<?php
 
 include 'layout/footer.php';
 
