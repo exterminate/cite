197dca6f611e24402ad516b38d76c6281aa66156
Ian <ian.coates@gmail.com>
2014-07-01 20:24:35 +0100

    writing the password reset

--- passreset.php
+++ passreset.php
@@ -6,12 +6,46 @@ include 'layout/header.php';
 
 if(Input::exists()) {
 	$validate = new Validate();
+	$validate->check($_POST, array( 
+                'email' => array(
+			'required' 	=> true,
+			'email' 	=> true			
+			)
+		)
+	);
+	if($vaidate->>passed()) {
+		// does this user exist?
+		// no
+		$dbHandler->getUser('email', Input::get('email'));
+		//check the above returns a user
+		
+		// yes
+		$newPassword = $dbHandler->getUniqueCode('stubId', 'links');
+		$dbHandler->updateNew('users', 'password', md5($newPassword), 'email', Input::get('email'));
+		$email = new EmailHandler();
+		$email->sendEmail(
+			'to',
+			'subject',
+			'message'
+		);
+	} else {
+		$_SESSION['error'] = "Sorry, this email isn't valid";
+	}
+
 	
 }
 
 ?>
 <h2>Reset password</h2>
 <p>We will e-mail you with a new password.</p>
+
+<?php
+if(isset($_SESSION['error'])) {
+	echo "<p>" . $_SESSION['error'] . "</p>";
+	unset($_SESSION['error']);
+}
+
+?>
 
 <form action="" method="POST">
 	<label for="email">Enter e-mail address:</label><br>
